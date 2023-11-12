bc2ec7740b70b465ebbdb8c43917bde641c69425
Konstantin <diel404@gmail.com>
2023-11-11 22:32:53 -0800

    Do TrafficLight app

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var trafficLightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        greenLightView.layer.cornerRadius = 64
+        yellowLightView.layer.cornerRadius = 64
+        redLightView.layer.cornerRadius = 64
+        
+        trafficLightButton.layer.cornerRadius = 10
+        
+        greenLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        redLightView.alpha = 0.3
     }
-
-
+    
+    enum TrafficLightStatus {
+        case off, green, yellow, red
+    }
+    
+    var currentTrafficLightStatus: TrafficLightStatus = .off
+    
+    @IBAction func trafficLightButtonDidTapped(_ sender: UIButton) {
+        switch currentTrafficLightStatus {
+        case .off:
+            greenLightView.alpha = 1
+            currentTrafficLightStatus = .green
+            trafficLightButton.setTitle("NEXT", for: .normal)
+        case .green:
+            greenLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            currentTrafficLightStatus = .yellow
+        case .yellow:
+            yellowLightView.alpha = 0.3
+            redLightView.alpha = 1
+            currentTrafficLightStatus = .red
+        case .red:
+            redLightView.alpha = 0.3
+            greenLightView.alpha = 1
+            currentTrafficLightStatus = .green
+        }
+    }
 }
 
